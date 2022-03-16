ef18a1193dc54f3c3d1e36abddf7d6df7e6640da
stporter <stporter2@gmail.com>
2022-03-16 15:03:29 -0400

    quiz-test

--- game.js
+++ game.js
@@ -142,4 +142,36 @@ getNewQuestion = () => {
 	});
 
 	availableQuestions.splice(questionsIndex, 1);
+
+	acceptingAnswers = true;
 };
+
+choices.forEach((choice) => {
+	choice.addEventListener('click', (e) => {
+		if (!acceptingAnswers) return;
+		acceptingAnswers = false;
+		const selectedChoice = e.target;
+		const selectedAnswer = selectedChoice.dataset['number'];
+
+		let classToApply =
+			selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+		if (classToApply === 'correct') {
+			incrementScore(SCORE_POINTS);
+		}
+
+		selectedChoice.parentElement.classList.add(classToApply);
+
+		setTimeout(() => {
+			selectedChoice.parentElement.classList.remove(classToApply);
+			getNewQuestion();
+		}, 1000);
+	});
+});
+
+incrementScore = (num) => {
+	score += num;
+	scoreText.innerText = score;
+};
+
+startGame();
